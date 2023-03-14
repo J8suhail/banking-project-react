d4756b6d723a45c088dfd44569373b1d7c3f02f6
J8suhail <124551426+J8suhail@users.noreply.github.com>
2023-03-14 15:26:36 +0530

    Add files via upload

--- src/Loginform.js
+++ src/Loginform.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import React from 'react';
 import Signup from './Signup';
+import axios from 'axios';
+import { Navigate } from "react-router-dom";
 
 class LoginForm extends React.Component{
 
@@ -8,13 +10,26 @@ class LoginForm extends React.Component{
         super();
         this.state={
             userName:"",
-            errorMessage:""
+            userpassword:"",
+            errorMessage:"",
+            users:[]
         }
     }
 
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(
+            res=>{
+                let tempData = res.data;
+                this.setState({users:tempData});
+            }
+        );
+    }
+
     validatePassword = (e) => {
-        let lowerCaseCheck =/^(\S)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]{10,16}$/;
-      
+        this.setState({userpassword:e.target.value});
+        let lowerCaseCheck =/^(\S)(?=.*[A-Z])(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])[a-zA-Z~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]{6,16}$/;
+
         if(e.target.value.match(lowerCaseCheck)){
           this.setState({
           errorMessage:""
@@ -25,17 +40,37 @@ class LoginForm extends React.Component{
           });}
       }
 
+      authenticateUser = (e)=>{
+        e.preventDefault();
+        
+        let flag=0;
+        this.state.users.forEach(user => {
+            if(user.email==this.state.userName && user.email==this.state.userpassword){
+                flag=1;
+            }
+        });
+        if(flag==1){
+            this.setState({errorMessage:""});
+            window.location.href = '/Dashboard';
+            }
+        else{
+            this.setState({errorMessage:"Invalid details"});
+            }   
+    }
+
     render(){
         return(
           
             <section id="loginbox">
-        <form action="#" name="loginForm">
-          <input type="text" placeholder="Username"/><br/>
+        <form action="#" name="loginForm" onSubmit={this.authenticateUser} >
+          <input type="text" placeholder="Username" onChange={(e)=>{
+                                            this.setState({userName:e.target.value})
+                                        }}/><br/>
           <input name="user_password" type="password" required minlength="10" placeholder="Password"  onChange={this.validatePassword}/><br/>
           <p>{this.state.errorMessage}</p>
           <input type="submit"/>
       </form>
-  
+  <br/>
         <Link to ='/Signup'> Open an Account</Link>
       </section>
       
